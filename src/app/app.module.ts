a0574d999a279dfb48f27728080259557fb4f9cc
georgemmp <machadogeorge.p@gmail.com>
2019-08-05 14:48:37 -0300

    ReactForms na página de cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
@@ -53,6 +53,7 @@ export function tokenGetter() {
       FormsModule,
       HttpClientModule,
       NgxGalleryModule,
+      ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
